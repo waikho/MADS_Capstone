edcf5362208608d93998589182843c09059741ce
Elaine Chao <61913073+dingdingmammy@users.noreply.github.com>
2023-04-03 14:07:03 +0800

    package ready

--- ML/modeling_main.py
+++ ML/modeling_main.py
@@ -24,6 +24,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split, KFold
 from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
 
+import warnings
+warnings.filterwarnings('ignore')
+
 
 def get_data(ticker):
 
@@ -231,7 +234,24 @@ def features_COMP_RS(dollar_bars, dollar_bars_COMP):
 
     return dollar_bars
 
-def modeling(symbol, events, dollar_bars, type='seq_boot_SVC', RANDOM_STATE = 42):
+def error_df(symbol, model_metrics, message):
+    """
+    helper function for modeling
+    output df to record down un-trainable cases
+    :param symbol: (str) ticker symbol
+    :param model_metrics: (dataframe) of model results
+    :param message: (str) error message from modeling 
+
+    """
+    model_metrics.loc[0] = np.nan
+    model_metrics['train_test'] = message
+    model_metrics['symbol'] = symbol
+    model_metrics['performed_on'] = datetime.datetime.now()
+    model_metrics['event_st_date'] = np.nan
+    model_metrics['event_en_date'] = np.nan
+    return model_metrics
+
+def modeling(symbol, events, dollar_bars, type='sequential_bootstrapping_SVC', RANDOM_STATE = 42):
     """
     modeling and perform hyperparameter tuning for a single stock dataframe
     dataframe comes with trend labels, meta labels, and additional features
@@ -242,14 +262,27 @@ def modeling(symbol, events, dollar_bars, type='seq_boot_SVC', RANDOM_STATE = 42
     :param RANDOM_STATE: (int) random state seed, default as 42
     :return clf, model_metrics: (classifier, dataframe) classification model for the ticker, model metrics for train and test dataset on a dataframe
     """
+    # df to store results
+    model_metrics = pd.DataFrame(columns = ['type', 'best_model', 'best_cross_val_score', 'recall', 'precision', 'accuracy','run_time'])
 
     # train test split
     col = ['open', 'high', 'low', 'close', 'volume', 'bin']
     dollar_bars = dollar_bars.dropna()
     X = dollar_bars.drop(col, axis=1)
     y = dollar_bars['bin']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False, random_state=RANDOM_STATE)
 
+    if len(y.unique()) < 2:
+        print('Only one class found. Unable to train model for {}.'.format(symbol))
+        error_df(symbol, model_metrics, 'No Train - single class')
+        return None, model_metrics
+    
+    try:
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False, random_state=RANDOM_STATE)
+    except ValueError:
+        print('ValueError - Unable to train model for {}'.format(symbol))
+        error_df(symbol, model_metrics, 'No Train - check train test split')
+        return None, model_metrics
+        
     # sample weights
     return_based_sample_weights = get_weights_by_return(events.loc[X_train.index], dollar_bars.loc[X_train.index, 'close'])
     return_based_sample_weights_test = get_weights_by_return(events.loc[X_test.index], dollar_bars.loc[X_test.index, 'close'])
@@ -266,14 +299,21 @@ def modeling(symbol, events, dollar_bars, type='seq_boot_SVC', RANDOM_STATE = 42
     cv_gen_purged = PurgedKFold(n_splits=n_splits, samples_info_sets=events.loc[X_train.index].t1)
 
     # get the best parameters for the selected model
-    best_params = cv.perform_grid_search(X_train, y_train, cv_gen_purged, 'f1', parameters, events, dollar_bars, type=type, sample_weight=return_based_sample_weights.values)
+    try:
+        best_params = cv.perform_grid_search(X_train, y_train, cv_gen_purged, 'f1', parameters, events, dollar_bars, type=type, sample_weight=return_based_sample_weights.values)
+    except ValueError:
+        print('Small sample size - Unable to train model for {}'.format(symbol))
+        error_df(symbol, model_metrics, 'No Train - small sample size')
+        return None, model_metrics
 
     # table to record down training results
-    model_metrics = pd.DataFrame(columns = ['type', 'best_model', 'best_cross_val_score', 'recall', 'precision', 'accuracy','run_time'])
+
     model_metrics = model_metrics.append(best_params, ignore_index = True)  
     model_metrics['train_test'] = 'Train'
     model_metrics['symbol'] = symbol
     model_metrics['performed_on'] = datetime.datetime.now()
+    model_metrics['event_st_date'] = X_train.index.min()
+    model_metrics['event_en_date'] = X_train.index.max()
 
     # perform testing and record score
     clf = best_params['best_model']
@@ -294,7 +334,9 @@ def modeling(symbol, events, dollar_bars, type='seq_boot_SVC', RANDOM_STATE = 42
         'run_time':t1-t0,
         'train_test':'Test',
         'symbol':symbol,
-        'performed_on':datetime.datetime.now()
+        'performed_on':datetime.datetime.now(),
+        'event_st_date': y_test.index.min(),
+        'event_en_date': y_test.index.max(),
     }
     model_metrics = model_metrics.append(test_results, ignore_index = True)
 
@@ -310,6 +352,9 @@ def get_one_model(ticker, config=config.pgSecrets):
 
     ticker_df = gd.getTicker_Daily(ticker, config=config)
     ticker_df.index = pd.to_datetime(ticker_df.tranx_date)
+    index_SPY = get_index(ticker_df, 'SPY')
+    index_SPY.index = pd.to_datetime(index_SPY.tranx_date)
+    ticker_df = ticker_df[ticker_df.index <= index_SPY.index.max()] #make sure ticker and index has same length
 
     # preprocessing
     dollar_bars = normalizing(ticker_df)
@@ -323,7 +368,6 @@ def get_one_model(ticker, config=config.pgSecrets):
     dollar_bars = features_serial_correlation(dollar_bars)
 
     # add features - relative strength to SPY
-    index_SPY = get_index(ticker_df, 'SPY')
     dollar_bars_SPY = bars.transform_index_based_on_dollarbar(dollar_bars, index_SPY)
     dollar_bars = features_SPY_RS(dollar_bars, dollar_bars_SPY)
 
@@ -334,6 +378,10 @@ def get_one_model(ticker, config=config.pgSecrets):
    
     # get the output model and train test metrics
     clf, model_metrics = modeling(ticker, events, dollar_bars)
+    model_metrics['rawdata_st_date'] = ticker_df.index.min()
+    model_metrics['rawdata_en_date'] = ticker_df.index.max()
+    model_metrics['SPY_st_date'] = index_SPY.index.min()
+    model_metrics['SPY_en_date'] = index_SPY.index.max()
 
     return clf, model_metrics
 
@@ -346,10 +394,13 @@ def get_multiple_models(ticker_lst, config=config.pgSecrets):
     """
     clfs = {}
     model_metrics_df = pd.DataFrame()
+    ln = str(len(ticker_lst))
 
-    for ticker in ticker_lst:
+    for i, ticker in enumerate(ticker_lst):
+        print('Processing {}/{} {}...'.format(str(i+1), ln, ticker))
         clf, model_metrics = get_one_model(ticker, config=config)
         clfs[ticker] = clf
         model_metrics_df = pd.concat([model_metrics_df, model_metrics], ignore_index=True)
-    
+        print('Modeling completed {}/{} {}'.format(str(i+1), ln, ticker))
+
     return clfs, model_metrics_df
